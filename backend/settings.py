9f5cb865a1fef23af5eeaba6e6a978354cd8f1f0
Matcon12 <matcon545@gmail.com>
2023-11-14 13:19:26 +0530

    backend deploy

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure--5d27lej!c+zsg+0ylf+1y)mz8pfz07t&h08hl6+^o22*z(7hd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 DATE_FORMAT = 'd-m-Y'
 DATE_INPUT_FORMATS = ['%d-%m-%Y', '%d/%m/%Y']
@@ -166,4 +167,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
